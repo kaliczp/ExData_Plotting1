1ff02cc535a4d8acd8c683f77c31d6fffe3075a5
Péter Kalicz <kaliczp@gmail.com>
2015-12-07 23:41:43 +0100

    Add .gitignore and finish first plot

--- plot1.R
+++ plot1.R
@@ -1,2 +1,4 @@
 ## Change your currend directory to the data directory
 pow.cons <- read.table("household_power_consumption.txt",na="?",skip=66637,nrows=2880,sep=";")
+pow.cons.head <- read.table("household_power_consumption.txt",na="?",nrows=1,sep=";")
+hist(pow.cons[,3],col=2,main="Global Active Power", xlab="Global Active Power (kilowatts)")
